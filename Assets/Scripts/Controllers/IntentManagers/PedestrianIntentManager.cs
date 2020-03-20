dcc6126ce6dd86914fd14cd498a1b61edadbf60b
LTSteve <steth1000@gmail.com>
2020-03-19 20:28:04 -0400

    hooked up some pedwalk stuff

---
+++ Assets/Scripts/Controllers/IntentManagers/PedestrianIntentManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PedestrianIntentManager : MonoBehaviour, IIntentManager
+{
+    public Vector2? moveIntent { get; private set; } = Vector2.zero;
+    public Vector3? moveTarget { get; set; } = Vector3.zero;
+    public bool unsetTarget { get; private set; }
+    public bool setTarget { get; private set; }
+    public float lookIntent { get; private set; } = 0f;
+    public bool action1 { get; private set; } = false;
+    public bool action2 { get; private set; } = false;
+    public bool action3 { get; private set; } = false;
+    public Vector3? mouseLocation { get; private set; } = null;
+
+    public float DoubletimeModifier = 2f;
+
+    public void UpdateIntent()
+    {
+        moveIntent = null;
+        moveTarget = null;
+        setTarget = false;
+        unsetTarget = false;
+    }
+}
